0d73b5a2d4fc99ceafb7e5ea5acc44618a8a25a2
laxman <lakshmana5296@gmail.com>
2023-03-19 16:24:17 +0530

    first commit

--- controllers/sales.controller.js
+++ controllers/sales.controller.js
@@ -8,6 +8,7 @@ const expressAsyncHandler=require("express-async-handler")
 
 const {Op,QueryTypes} =require("sequelize")
 const { sequelize } = require("../database/db.config")
+const { stat } = require("fs")
 
 
 //controller for initialization of database
@@ -32,9 +33,7 @@ exports.statisticsByMonth=expressAsyncHandler(async(req,res)=>{
     //pattern
     let pattern="_____"+month+"%"
     //query
-    let query="select * from sales where monthname(dateOfSale)='"+month+"'"
-    //running query
-    let result= await sequelize.query(query, { type: QueryTypes.SELECT ,
+    let result= await sequelize.query("select * from sales where monthname(dateOfSale)='"+month+"'", { type: QueryTypes.SELECT ,
     model:Sales})
     
     //send response
@@ -48,10 +47,9 @@ exports.statisticsByCategory=expressAsyncHandler(async(req,res)=>{
     let month=req.params.month
     //pattern
     let pattern="_____"+month+"%"
-    //query
-    let query="select category,count(*) as items from sales where monthname(dateOfSale)='"+month+"' group by category"
+
     //running query
-    let result= await sequelize.query(query, { type: QueryTypes.SELECT ,
+    let result= await sequelize.query("select category,count(*) as items from sales where monthname(dateOfSale)='"+month+"' group by category", { type: QueryTypes.SELECT ,
     model:Sales})
     
     //send response
@@ -65,36 +63,69 @@ exports.statisticsOfItems=expressAsyncHandler(async(req,res)=>{
     let month=req.params.month
     //pattern
     let pattern="_____"+month+"%"
-    //query
-    let query="select * from sales where monthname(dateOfSale)='"+month+"' group by category"
-    //running query
-    let result= await sequelize.query(query, { type: QueryTypes.SELECT ,
+    let object={}
+
+    //Running queries for all intervals
+    for(let i=0;i<9;i++){
+       let  start=i*100+1
+       if(i==0)
+       start=start-1
+       let end=100+i*100
+
+        //running query
+        let result= await sequelize.query("select * from sales where monthname(dateOfSale)='"+month+"'&& price between "+start+" and "+end, { type: QueryTypes.SELECT ,
+        model:Sales})
+            object[start+"-"+end]=result.length
+            
+        }
+        //running query
+    let result= await sequelize.query("select * from sales where monthname(dateOfSale)='"+month+"'&& price>900", { type: QueryTypes.SELECT ,
     model:Sales})
-    
+
+    object["900 -above"]=result.length
     //send response
-    res.send({message:"Data fetched sucessfully",payload:result})
+    res.send({message:"Data fetched sucessfully",payload:object})
 })
 
 
 //final report
 exports.finalReport=expressAsyncHandler(async(req,res)=>{
     
-      //month from parameters
+    //month from parameters
     let month=req.params.month
     //pattern
     let pattern="_____"+month+"%"
-    //query
-    let query="select * from sales where monthname(dateOfSale)='"+month+"'"
-    //running query
-    //query
-    let query2="select category,count(*) as items from sales where monthname(dateOfSale)='"+month+"' group by category"
-    //running query
-    let montlyreport= await sequelize.query(query, { type: QueryTypes.SELECT ,
-    model:Sales})
-    let pieChart= await sequelize.query(query2, { type: QueryTypes.SELECT ,
-        model:Sales})
 
+    //running query for Montly report API
+    let montlyreport= await sequelize.query("select * from sales where monthname(dateOfSale)='"+month+"'", { type: QueryTypes.SELECT ,
+    model:Sales})
+
+    //running query for Pie chart API
+    let pieChart= await sequelize.query("select category,count(*) as items from sales where monthname(dateOfSale)='"+month+"' group by category", { type: QueryTypes.SELECT ,
+        model:Sales})
+    
+    //Running qqueries for Bar chart
+    //Running queries for all intervals 
+    let object={}
+    for(let i=0;i<9;i++){
+           let  start=i*100+1
+           if(i==0)
+           start=start-1
+           let end=100+i*100
+    
+            let query="select * from sales where monthname(dateOfSale)='"+month+"'&& price between "+start+" and "+end
+                //running query
+            let result= await sequelize.query(query, { type: QueryTypes.SELECT ,
+            model:Sales})
+                object[start+"-"+end]=result.length
+                
+            }
+         //running query
+        result= await sequelize.query("select * from sales where monthname(dateOfSale)='"+month+"'&& price>900", { type: QueryTypes.SELECT ,
+        model:Sales})
+        
+        object["900 -above"]=result.length
     
     //send response
-    res.send({message:"Data fetched sucessfully",payload:{report:montlyreport,pieChart:pieChart}})
+    res.send({message:"Data fetched sucessfully",payload:{report:montlyreport,pieChart:pieChart,barChart:object}})
 })
